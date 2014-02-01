862d5a2afb9d7559412754a0fa67e48e37df540a
Kedar Amladi <kamladi@gmail.com>
2014-02-01 16:46:46 -0500

    get request

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,18 @@
-var socket = io.connect('http://localhost:8080');
-
-console.log(window.location.pathname);
+var socket = io.connect('http://localhost:8000');
+var roomId =
+console.log();
 console.log("THIS IS A TEST LOOKOUT");
 socket.emit('joinRoom', 0);
 
 socket.on('timestamp', function(data) {
-  console.log("timestamp");
+  console.log("received timestamp");
+  window.timestamp = data;
   console.dir(data);
 });
 
 socket.on('playlist', function(data) {
-  console.log("playlist:");
-  console.dir(data);
+  console.log("received playlist");
+  window.playlist = data;
 });
 
 //Soundcloud stuff
@@ -24,8 +25,14 @@ SC.initialize({
 $("#searchButton").click(soundcloudSearch('satisfaction'));
 
 function soundcloudSearch (query) {
+	/*
 	SC.get('/tracks', { q: query}, function(tracks) {
 		console.log(tracks);
 	});
+*/
+	$.getJSON('/search', {q: encodeURIComponent(query)}, function(data) {
+		console.log("search complete");
+		console.log(data);
+	})
 }
 
